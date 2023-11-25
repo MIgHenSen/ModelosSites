7b135fcee9429ea98baad263a15a0fe80d802d96
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-24 22:24:45 -0300

    Update script1.js

--- Modelos Layout/_js/script1.js
+++ Modelos Layout/_js/script1.js
@@ -1,10 +1,7 @@
-/* 'addEventListener' permite que um evento específico seja configurado,
-neste caso, o evento "click" */
 openMenu.addEventListener('click', () => {
 	menu.style.display = "flex"
 	menu.style.right = (menu.offsetWidth * -1) + 'px'
 	
-	/* 'setTimeout' define um tempo de espera para as ações definidas */
 	setTimeout(()=> {
 		menu.style.opacity = '1'
 		menu.style.right = "0"
@@ -16,7 +13,6 @@ closeMenu.addEventListener('click', () => {
 	menu.style.opacity = '0'
 	menu.style.right = (menu.offsetWidth * -1) + 'px'
 	setTimeout(()=> {
-		/* 'removeAttribute' retira a propriedade especificada */
 		menu.removeAttribute('style')
 		openMenu.removeAttribute('style')
 	}, 200);
